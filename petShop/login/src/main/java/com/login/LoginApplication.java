18eb02ff6073ea97a7499833f49856b7f876f04b
NoFatter <916803397@qq.com>
2022-06-21 10:10:02 +0800

    test

---
+++ petShop/login/src/main/java/com/login/LoginApplication.java
@@ -0,0 +1,16 @@
+package com.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Project name:petShop
+ * Author: NoFat
+ * Create time:2022/6/20 21:38
+ **/
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LoginApplication.class);
+    }
+}
---
+++ petShop/petManage/src/main/java/com/petManage/PetManageApplication.java
@@ -0,0 +1,16 @@
+package com.petManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Project name:petShop
+ * Author: NoFat
+ * Create time:2022/6/20 21:32
+ **/
+@SpringBootApplication
+public class PetManageApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PetManageApplication.class);
+    }
+}
