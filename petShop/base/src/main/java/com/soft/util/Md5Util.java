7f69508edcf19fb812c5585d5c39cf5f07532f47
NoFatter <916803397@qq.com>
2022-06-22 08:44:18 +0800

    Create Md5Util.java

---
+++ petShop/base/src/main/java/com/soft/util/Md5Util.java
@@ -0,0 +1,30 @@
+package com.soft.util;
+
+import java.security.MessageDigest;
+
+/**
+ * Project name:petShop
+ * Author: NoFat
+ * Create time:2022/6/22 8:34
+ **/
+//public class Md5Util {
+//    private static String getMd5Encode(String str) throws Exception{
+//        MessageDigest md5 = null;
+//        try {
+//            md5 = MessageDigest.getInstance("MD5");
+//        }catch (Exception e){
+//            return "";
+//        }
+////        byte[] byteArray = inStr
+//        byte[] md5Bytes = md5.digest(byteArray);
+//        StringBuffer hexValue = new StringBuffer();
+//        for (int i = 0; i < md5Bytes.length; i++) {
+//            int val = ((int) md5Bytes[i]) & 0xff;
+//            if (val < 16) {
+//                hexValue.append("0");
+//            }
+//            hexValue.append(Integer.toHexString(val));
+//        }
+//        return hexValue.toString();
+//    }
+//}
