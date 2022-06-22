cccb710499d4d608dd91bd4db4d002e07bf55b53
NoFatter <916803397@qq.com>
2022-06-22 09:32:13 +0800

    Update Md5Util.java

--- petShop/base/src/main/java/com/soft/util/Md5Util.java
+++ petShop/base/src/main/java/com/soft/util/Md5Util.java
@@ -1,5 +1,6 @@
 package com.soft.util;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 /**
@@ -7,24 +8,27 @@ import java.security.MessageDigest;
  * Author: NoFat
  * Create time:2022/6/22 8:34
  **/
-//public class Md5Util {
-//    private static String getMd5Encode(String str) throws Exception{
-//        MessageDigest md5 = null;
-//        try {
-//            md5 = MessageDigest.getInstance("MD5");
-//        }catch (Exception e){
-//            return "";
-//        }
-////        byte[] byteArray = inStr
-//        byte[] md5Bytes = md5.digest(byteArray);
-//        StringBuffer hexValue = new StringBuffer();
-//        for (int i = 0; i < md5Bytes.length; i++) {
-//            int val = ((int) md5Bytes[i]) & 0xff;
-//            if (val < 16) {
-//                hexValue.append("0");
-//            }
-//            hexValue.append(Integer.toHexString(val));
-//        }
-//        return hexValue.toString();
-//    }
-//}
+public class Md5Util {
+    private static String md5Encode(String str) throws Exception{
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        }catch (Exception e){
+            return "";
+        }
+        byte[] byteArray = str.getBytes(StandardCharsets.UTF_8);
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuilder hexValue = new StringBuilder();
+        for (byte md5Byte : md5Bytes) {
+            int val = ((int) md5Byte) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+    public static String getEncode(String pwd) throws Exception{
+        return md5Encode(md5Encode(pwd));
+    }
+}
