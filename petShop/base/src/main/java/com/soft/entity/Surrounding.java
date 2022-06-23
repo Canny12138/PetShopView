af67faaa05995d61ce4ecd37ff96a1aced07a65a
NoFatter <916803397@qq.com>
2022-06-23 17:15:14 +0800

    add surrounding module

---
+++ petShop/base/src/main/java/com/soft/entity/Surrounding.java
@@ -0,0 +1,19 @@
+package com.soft.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * Project name:petShop
+ * Author: NoFat
+ * Create time:2022/6/23 17:07
+ **/
+@Data
+@TableName("surrounding")
+public class Surrounding {
+    @TableId
+    private String surroundingId;
+    private String surroundName;
+    private int surroundType;
+}
