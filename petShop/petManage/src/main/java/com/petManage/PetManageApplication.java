7651a21c67568ff68f4125cf7bc02a22f344995b
NoFatter <916803397@qq.com>
2022-06-27 10:27:55 +0800

    update storeService

--- petShop/petManage/src/main/java/com/petManage/PetManageApplication.java
+++ petShop/petManage/src/main/java/com/petManage/PetManageApplication.java
@@ -2,9 +2,6 @@ package com.petManage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Project name:petShop
---
+++ petShop/port.md
@@ -0,0 +1,15 @@
+# gateway
+port:9001
+# login
+port:8001
+# petManage
+port:8003
+# userManage
+port:8004
+# surroundingManage
+port:8005
+# goodManage
+port:8006
+# storeService
+port:8007
+
