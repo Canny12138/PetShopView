505d077a1da2eca605981485c416055b309bb183
NoFatter <916803397@qq.com>
2022-06-30 15:56:39 +0800

    add rocketmq

---
+++ petShop/example/src/main/java/com/example/controller/SyncProducer.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+import javax.validation.groups.Default;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Project name:petShop
+ * Author: NoFat
+ * Create time:2022/6/30 14:56
+ **/
+public class SyncProducer {
+    public static void main(String[] args) throws Exception{
+        DefaultMQProducer producer = new DefaultMQProducer("group_test");
+
+        producer.setNamesrvAddr("localhost:9876");
+        producer.setSendMsgTimeout(1000000);
+        producer.start();
+        for(int i=0;i<10;i++){
+            Message msg = new Message("TopicTest","TagA",("hello rocketmq"+i).getBytes(StandardCharsets.UTF_8));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n",sendResult);
+        }
+        producer.shutdown();
+    }
+}
